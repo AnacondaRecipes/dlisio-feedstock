a215dd8aad18be0a6c5f8db8883490388379b34d
Bradley Kreider <user.email=1699478+bkreider@users.noreply.github.com>
2022-07-25 12:12:55 -0400

    Services: first build

---
+++ recipe/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir ${SRC_DIR}/build
+cd ${SRC_DIR}/build
+cmake .. -DBUILD_SHARED_LIBS=ON -DLFP_FMT_HEADER_ONLY=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DPYTHON_EXECUTABLE=${PREFIX}/bin/python -DCMAKE_INSTALL_PREFIX=${PREFIX}
+
+make -j${CPU_COUNT:-4}
+make install
