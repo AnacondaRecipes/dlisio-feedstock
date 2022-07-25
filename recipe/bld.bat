59ab15b613322f949a1f478b9ae8eedd8b854826
Bradley Kreider <user.email=1699478+bkreider@users.noreply.github.com>
2022-07-25 15:33:42 -0400

    Fix host/build sections

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,12 +10,10 @@ cmake ^
   -S %SRC_DIR% ^
   -B %SRC_DIR%\build ^
   -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
-  -DLFP_FMT_HEADER_ONLY=ON ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DBUILD_SHARED_LIBS=ON ^
-  -DLFP_FMT_HEADER_ONLY=ON ^
-  -DBUILD_TESTING=OFF 
-  -DPYTHON_EXECUTABLE=${PREFIX}/bin/python ^
+  -DBUILD_TESTING=OFF ^
+  -DPYTHON_EXECUTABLE=${PREFIX}/bin/python
 
 cd build
 if errorlevel 1 exit /b 1
